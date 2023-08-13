32d731ef359bc91141c362660075c49016b7c545
Leandro Hernandez <l.hernandez071999@gmail.com>
2023-08-13 20:12:13 -0300

    Se crean los modelos restaurante y admin, ademas de sus controllers

--- controllers/sessionController.php
+++ controllers/sessionController.php
@@ -1,1 +1,38 @@
 <?php
+class LoginController {
+    public function index() {
+        include 'views/login_view.php';
+    }
+
+    public function login() {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // Realizar la validación del usuario y contraseña aquí (ejemplo simplificado)
+        if ($username === 'usuario' && $password === 'contraseña') {
+            session_start();
+            $_SESSION['usuario'] = $username;
+
+            // Crear cookie de sesión
+            setcookie('usuario', $username, time() + 3600, '/'); // Cookie válida por 1 hora
+
+            header('Location: index.php');
+        } else {
+            echo 'Credenciales inválidas';
+        }
+    }
+
+    public function logout() {
+        session_start();
+        session_unset();
+        session_destroy();
+        
+       
+        setcookie('usuario', '', time() - 3600, '/');
+
+        header('Location: index.php');
+    }
+}
+
+
+?>
